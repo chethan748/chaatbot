8cbcff90c88df94b32924b71cd31dda2d9906df1
chethan748 <chethanm854@gmail.com>
2026-09-03 16:19:40 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,8 @@
+import streamlit as st
+a=st.chat_input("enter your command :: ")
+if a:
+	st.chat_message("user").write(a)
+	if a.lower()=="hii":
+		st.chat_message("ai").write("hello")
+	elif a.lower()=="byee":
+		st.chat_message("ai").write("good byee")
